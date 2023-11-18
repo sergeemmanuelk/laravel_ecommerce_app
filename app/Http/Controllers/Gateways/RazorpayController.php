d830e9c45a40496877c92f443f28e228d4815f1b
sergeemmanuelk <serge_em@outlook.fr>
2023-11-18 14:24:53 +0000

    feat: razorpay integration

---
+++ app/Http/Controllers/Gateways/RazorpayController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Gateways;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RazorpayController extends Controller
+{
+    public function payment()
+    {
+
+    }
+}
