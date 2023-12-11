e53e43881e7102fe82f5f5fbb117768795684634
sergeemmanuelk <serge_em@outlook.fr>
2023-12-11 18:28:30 +0000

    implement frontend: setup - template mastering

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('frontend.partials.head')
+</head>
+
+<body>
+
+<!--============================
+    HEADER START
+==============================-->
+@include('frontend.partials.header')
+<!--============================
+    HEADER END
+==============================-->
+
+
+<!--============================
+    MAIN MENU START
+==============================-->
+@include('frontend.partials.menu')
+<!--============================
+    MAIN MENU END
+==============================-->
+
+
+<!--============================
+    MOBILE MENU START
+==============================-->
+@include('frontend.partials.mobile-menu')
+<!--============================
+    MOBILE MENU END
+==============================-->
+
+
+<!--==========================
+    POP UP START
+===========================-->
+<!-- <section id="wsus__pop_up">
+    <div class="wsus__pop_up_center">
+        <div class="wsus__pop_up_text">
+            <span id="cross"><i class="fas fa-times"></i></span>
+            <h5>get up to <span>75% off</span></h5>
+            <h2>Sign up to E-SHOP</h2>
+            <p>Subscribe to the <b>E-SHOP</b> market newsletter to receive updates on special offers.</p>
+            <form>
+                <input type="email" placeholder="Your Email" class="news_input">
+                <button type="submit" class="common_btn">go</button>
+                <div class="wsus__pop_up_check_box">
+                </div>
+            </form>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault11">
+                <label class="form-check-label" for="flexCheckDefault11">
+                    Don't show this popup again
+                </label>
+            </div>
+        </div>
+    </div>
+</section> -->
+<!--==========================
+    POP UP END
+===========================-->
+
+<!--==========================
+    MAIN PART START
+===========================-->
+@yield('content')
+<!--==========================
+    MAIN PART END
+===========================-->
+
+<!--============================
+    FOOTER PART START
+==============================-->
+@include('frontend.partials.footer')
+<!--============================
+    FOOTER PART END
+==============================-->
+
+
+<!--============================
+    SCROLL BUTTON START
+==============================-->
+<div class="wsus__scroll_btn">
+    <i class="fas fa-chevron-up"></i>
+</div>
+<!--============================
+    SCROLL BUTTON  END
+==============================-->
+
+@include('frontend.partials.scripts')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
